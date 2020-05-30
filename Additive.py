5773873c72ccb72536aaab71dc24771d05b4bf66
Nick-Wue <40614116+Nick-Wue@users.noreply.github.com>
2020-05-30 12:42:32 +0200

    Add files via upload

--- Additive.py
+++ Additive.py
@@ -3,14 +3,15 @@ def convert(text): #converts text to ASCII representations for easier handling
     return [ord(i) for i in text]
 def deconvert(numbers):
     return [chr(i) for i in numbers]
-def additive(key, text):
+def additive(key, text,mode):
     text = convert(text)
     key = key % 26
+    if mode == "D":	#for decrypt set "D" as 4th Argument
+        key = -key
     for x in range(len(text)):
-        if text[x] == 13 or text[x] == 32:  #32 for new line and 13 for space in ASCII
+        if text[x] == ord(" ") or text[x] == ord("\n"):  #ignore newline and space
             continue
-        text[x] =((text[x] + key) % 91)
-        if text[x] < 65: text[x] = text[x] + 65 #to keep in ASCII range between A and Z
+        text[x] =((text[x] - 65 + key)% 26 + 65 )  
     text = deconvert(text) #convert back to textrepresentation
     return "".join(text)
 def bruteforce(crypt):
@@ -19,6 +20,6 @@ def bruteforce(crypt):
         print("".join(solution))
 input = open(sys.argv[1], "r")
 output = open(sys.argv[2], "w")
-output.write(additive(sys.argv[3], input.read()))
+output.write(additive(int(sys.argv[3]), input.read(), sys.argv[4]))
 input.close()
 output.close()
