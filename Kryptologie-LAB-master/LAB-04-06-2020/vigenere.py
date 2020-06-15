d426ae7131b010372ba3b774f5cd090fe47768ae
Nick-Wue <40614116+Nick-Wue@users.noreply.github.com>
2020-06-15 23:13:22 +0200

    nicht komplett fertig.

--- Kryptologie-LAB-master/LAB-04-06-2020/vigenere.py
+++ Kryptologie-LAB-master/LAB-04-06-2020/vigenere.py
@@ -1,4 +1,11 @@
 import sys
+import math
+
+def calc_language_e_dictionary(language):
+    lang_dict = get_count_dict(language)
+    for entry in lang_dict:
+        lang_dict[entry] /= len(language)
+    return lang_dict
 
 
 def calc_rauheit(text):
@@ -14,17 +21,72 @@ def calc_rauheit(text):
     return sum_ - (1 / 128)
 
 
-def brute_rauheit(text):
-    for iterator in range(1, 100):
+def get_count_dict(iterable):  #count unique elements in dictionary (element : count)
+    lang_dict = {}
+    for letter in iterable:
+        if letter not in lang_dict:
+            lang_dict[letter] = 1
+        else:
+            lang_dict[letter] += 1
+    return lang_dict
+
+
+def brute_rauheit(text): #calc rauheit to every possible blocklength,return list of average rauheitsgrade
+    average_list = []
+    for blocklength in range(1, 100):
         mr_sum = 0
-        for j in range(1, iterator + 1):
-            iter_text = text[j::iterator]
+        for blocksteps in range(1, blocklength + 1):
+            iter_text = text[blocksteps::blocklength]
             mr_sum += calc_rauheit(iter_text)
-        avg = mr_sum / iterator
-        print("Blocklength: {}, MR : {}".format(iterator, avg)) #TODO: Alle MR in Liste, max berechnen, kgt finden
-                                                                # -> Wahrscheinlich Blocklänge.
+        avg_mr = mr_sum / blocklength
+        average_list.append(avg_mr)
+        print("Blocklength: {}, MR : {}".format(blocklength, avg_mr))
+    return average_list
 
 
-text = open("encrypted-lorem-3.txt", "r")
-brute_rauheit(text.read())
+def get_key(text, blocklength):
+    key_list = list()
+    for iterator in range(blocklength):
+        blocked_text = text[iterator::blocklength]
+        chi_list = list()
+        for i in range(0, 128):
+            decrypted_text_list = [chr((ord(letter) - i) % 128) for letter in blocked_text]
+            chi_list.append(calc_chi(get_count_dict(decrypted_text_list)))
+        key_list.append(get_index_of_min(chi_list)) #for smallest chi the textcolumn is nearest to the original
+    return key_list                                  # distribution of letters
+
+
+def find_smallest_mr(average_list):# because smallest mr is likely to be the keylenght this works most likely
+    for i, entry in enumerate(average_list):
+        if entry > 0.06:    #because rauheit of Laurem about 0.06 this works
+            return i + 1
+
+
+def calc_chi(count_dict): #calculate a metric that is small if the number of letters is close to the expected number in
+    chi = 0                # of letters in the original language
+    chi_sum = 0
+    for i in count_dict:
+        if i not in LANG_DICT:
+            expected_count = 0
+        else:
+            expected_count = LANG_DICT[i]
+        count = count_dict[i]
+        chi = (count - expected_count) ** 2
+        chi_sum += chi
+    return math.sqrt(chi_sum)
+
+def get_index_of_min(list):
+    for i, value in enumerate(list):
+        if value == min(list):
+            return i
+
+
+LANGUAGE = open("lorem.txt", "r").read()    #Language is Lorem in this case but can be changed if needed
+LANG_DICT = calc_language_e_dictionary(LANGUAGE)
+
+
+
+text = open("encrypted-lorem-2.txt", "r").read()
+blocklength = find_smallest_mr(brute_rauheit(text))
+print(get_key(text, blocklength))
 
